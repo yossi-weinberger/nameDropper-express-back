826ecf7f4be7e1a0009e318674303c872fee8609
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-05-31 13:25:22 +0300

    deleted dead code

--- categories.mjs
+++ categories.mjs
@@ -25,6 +25,7 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get category by id
 router.get("/:id", async (req, res, next) => {
   try {
     const categoryId = req.params.id;
@@ -58,63 +59,6 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-// router.get("/:id", async (req, res, next) => {
-//   const values = await categoriesCollection.findOne({
-//     _id: new ObjectId(req.params.id),
-//   });
-//   res.json({ data: values, status: "success" });
-// });
-
-// Get subcategories for a specific category
-// router.get("/:categoryId", async (req, res) => {
-//   const { categoryId } = req.params;
-//   try {
-//     const values = await valuesCollection.find({
-//       "category._id": mongoose.Types.ObjectId(categoryId),
-//     });
-//     res.json({ data: values });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// router.get("/:categoryId", async (req, res) => {
-//   try {
-//     const subcategories = await categoriesCollection
-//       .find({ parentId: req.params.categoryId })
-//       .toArray();
-//     res.json({ data: subcategories, status: "success" });
-//   } catch (error) {
-//     res.json({ error: error, status: "Error" });
-//   }
-// });
-
-// Get values for a specific category
-router.get("/:categoryName/values", async (req, res) => {
-  try {
-    const { categoryName } = req.params;
-    const values = await categoriesCollection
-      .find({ "category.name": categoryName })
-      .toArray();
-    res.json({ data: values, status: "success" });
-  } catch (error) {
-    res.json({ error: error, status: "Error" });
-  }
-});
-
-// Get a specific item by subcategory and item ID
-
-router.get("/:subcategoryId/:id", async (req, res) => {
-  try {
-    const item = await categoriesCollection.findOne({
-      _id: new ObjectId(req.params.id),
-      parentId: req.params.subcategoryId,
-    });
-    res.json({ data: item, status: "success" });
-  } catch (error) {
-    res.json({ error: error, status: "Error" });
-  }
-});
-
 // Update a category
 router.patch("/:id", async (req, res) => {
   const categories = await categoriesCollection.updateOne(
@@ -133,68 +77,3 @@ router.delete("/:id", async (req, res) => {
 });
 
 export default router;
-
-// import express from "express";
-// import { Router } from "express";
-// import { ObjectId } from "mongodb";
-// import { categoriesCollection } from "./mongoConnect.mjs";
-// const router = Router();
-
-// // Create a new category
-// router.post("/", async (req, res) => {
-//   try {
-//     await categoriesCollection.insertOne(req.body);
-
-//     res.status(201).send("category created successfully");
-//   } catch (error) {
-//     res.json({ error: error, status: "Error" });
-//   }
-// });
-
-// router.post("/many", async (req, res) => {
-//   try {
-//     // console.log(req.body);
-//     await categoriesCollection.insertMany(req.body);
-
-//     res.send("categories created successfully");
-//   } catch (error) {
-//     res.json({ error: error, status: "Error" });
-//   }
-// });
-
-// // Get all categories
-// router.get("/", async (req, res) => {
-//   try {
-//     const categories = await categoriesCollection.find().toArray();
-//     res.json({ data: categories, status: "success" });
-//   } catch (error) {}
-// });
-
-// // Get a single category
-// router.get("/:id", async (req, res, next) => {
-//   const categories = await categoriesCollection.findOne({
-//     _id: new ObjectId(req.params.id),
-//   });
-//   res.json({ data: categories, status: "success" });
-// });
-
-// // Update a category
-// router.patch("/:id", async (req, res) => {
-//   const categories = await categoriesCollection.updateOne(
-//     {
-//       _id: new ObjectId(req.params.id),
-//     },
-//     { $set: req.body }
-//   );
-//   res.json({ data: categories, status: "success" });
-// });
-
-// // Delete a category
-// router.delete("/:id", async (req, res) => {
-//   const categories = await categoriesCollection.deleteOne({
-//     _id: new ObjectId(req.params.id),
-//   });
-//   res.json({ data: categories, status: "success" });
-// });
-
-// export default router;
--- values.mjs
+++ values.mjs
@@ -5,16 +5,6 @@ import { valuesCollection } from "./mongoConnect.mjs";
 
 const router = Router();
 
-// Create a new category
-// router.post("/", async (req, res) => {
-//   try {
-//     await categoriesCollection.insertOne(req.body);
-//     res.status(201).send("category created successfully");
-//   } catch (error) {
-//     res.json({ error: error, status: "Error" });
-//   }
-// });
-
 // Get all values
 router.get("/", async (req, res) => {
   try {
@@ -24,14 +14,6 @@ router.get("/", async (req, res) => {
     res.json({ error: error, status: "Error" });
   }
 });
-
-// Get a single value
-// router.get("/:id", async (req, res, next) => {
-//   const values = await valuesCollection.findOne({
-//     _id: new ObjectId(req.params.id),
-//   });
-//   res.json({ data: values, status: "success" });
-// });
 
 router.get("/:id", async (req, res, next) => {
   try {
@@ -51,148 +33,4 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-// Get subcategories for a specific category
-// router.get("/categories/:categoryId/values", async (req, res) => {
-//   const { categoryId } = req.params;
-//   try {
-//     const values = await Value.find({
-//       "category.name": mongoose.Types.ObjectId(categoryId),
-//     });
-//     res.json({ data: values });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// Get subcategories for a specific category
-// router.get("/:categoryId", async (req, res) => {
-//   try {
-//     const subcategories = await valuesCollection
-//       .find({ parentId: req.params.categoryId })
-//       .toArray();
-//     res.json({ data: values, status: "success" });
-//   } catch (error) {
-//     res.json({ error: error, status: "Error" });
-//   }
-// });
-
-// Get values for a specific category
-// router.get("/:categoryId", async (req, res) => {
-//   try {
-//     const { categoryName } = req.params;
-//     const values = await valuesCollection
-//       .find({ "category.name": categoryName })
-//       .toArray();
-//     res.json({ data: values, status: "success" });
-//   } catch (error) {
-//     res.json({ error: error, status: "Error" });
-//   }
-// });
-
-// Get a specific item by subcategory and item ID
-
-// router.get("/:subcategoryId/values", async (req, res) => {
-//   try {
-//     const { subcategoryId } = req.params;
-//     const values = await categoriesCollection
-//       .find({ "category.name": subcategoryId })
-//       .toArray();
-//     res.json({ data: values, status: "success" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message, status: "Error" });
-//   }
-// });
-// router.get("/:subcategoryId/:id", async (req, res) => {
-//   try {
-//     const item = await categoriesCollection.findOne({
-//       _id: new ObjectId(req.params.id),
-//       parentId: req.params.subcategoryId,
-//     });
-//     res.json({ data: item, status: "success" });
-//   } catch (error) {
-//     res.json({ error: error, status: "Error" });
-//   }
-// });
-
-// Update a category
-// router.patch("/:id", async (req, res) => {
-//   const categories = await categoriesCollection.updateOne(
-//     { _id: new ObjectId(req.params.id) },
-//     { $set: req.body }
-//   );
-//   res.json({ data: categories, status: "success" });
-// });
-
-// // Delete a category
-// router.delete("/:id", async (req, res) => {
-//   const categories = await categoriesCollection.deleteOne({
-//     _id: new ObjectId(req.params.id),
-//   });
-//   res.json({ data: categories, status: "success" });
-// });
-
 export default router;
-
-// import express from "express";
-// import { Router } from "express";
-// import { ObjectId } from "mongodb";
-// import { categoriesCollection } from "./mongoConnect.mjs";
-// const router = Router();
-
-// // Create a new category
-// router.post("/", async (req, res) => {
-//   try {
-//     await categoriesCollection.insertOne(req.body);
-
-//     res.status(201).send("category created successfully");
-//   } catch (error) {
-//     res.json({ error: error, status: "Error" });
-//   }
-// });
-
-// router.post("/many", async (req, res) => {
-//   try {
-//     // console.log(req.body);
-//     await categoriesCollection.insertMany(req.body);
-
-//     res.send("categories created successfully");
-//   } catch (error) {
-//     res.json({ error: error, status: "Error" });
-//   }
-// });
-
-// // Get all categories
-// router.get("/", async (req, res) => {
-//   try {
-//     const categories = await categoriesCollection.find().toArray();
-//     res.json({ data: categories, status: "success" });
-//   } catch (error) {}
-// });
-
-// // Get a single category
-// router.get("/:id", async (req, res, next) => {
-//   const categories = await categoriesCollection.findOne({
-//     _id: new ObjectId(req.params.id),
-//   });
-//   res.json({ data: categories, status: "success" });
-// });
-
-// // Update a category
-// router.patch("/:id", async (req, res) => {
-//   const categories = await categoriesCollection.updateOne(
-//     {
-//       _id: new ObjectId(req.params.id),
-//     },
-//     { $set: req.body }
-//   );
-//   res.json({ data: categories, status: "success" });
-// });
-
-// // Delete a category
-// router.delete("/:id", async (req, res) => {
-//   const categories = await categoriesCollection.deleteOne({
-//     _id: new ObjectId(req.params.id),
-//   });
-//   res.json({ data: categories, status: "success" });
-// });
-
-// export default router;
